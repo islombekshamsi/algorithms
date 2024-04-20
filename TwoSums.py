a9cee875d8770ebca9a19dc7db57d3fe94a5d161
islombekshamsi <167647904+islombekshamsi@users.noreply.github.com>
2024-04-21 00:47:38 +0500

    Add files via upload

---
+++ TwoSums.py
@@ -0,0 +1,10 @@
+class Solution:
+    def __init__(self, nums: list[int], target: int) -> List[int]:
+        self.nums = nums
+        self.target = target
+
+    def twoSum(self):
+        for index in range(len(self.nums) - 1):
+            if self.nums[index] + self.nums[index + 1] == self.target:
+                return [index, index + 1]
+        return []
