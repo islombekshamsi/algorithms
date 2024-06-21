ee6c97a6917dacd5d55fc8fd81d958f290c293b7
islombekshamsiev <167647904+islombekshamsi@users.noreply.github.com>
2024-06-21 18:41:34 +0500

    ValidParentheses.py

--- ValidParentheses.py
+++ ValidParentheses.py
@@ -1,20 +1,18 @@
 class Solution:
     def __init__(self, s):
         self.s = s
-    def parentheses(self):
+        def parentheses(self):
         prnts = {
             "(": ")",
             "[": "]",
             "{": "}"
         }
         length = len(self.s)
-        stack = []
-        for i in range(length):
-            if self.s[i] in prnts:
-                stack.append(self.s[i])
-            elif not stack or prnts[stack.pop()] != self.s[i]:
-                return False
-        return len(stack) == 0
+        flag = True
+        for i in range(0, length, 2):
+            if self.s[i+1] != prnts[self.s[i]]:
+                flag = False
+        return flag
 
 
 par = Solution("(}")
